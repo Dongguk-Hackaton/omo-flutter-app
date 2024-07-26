6478390403def95a9369a035a931def2c5d93d77
kkr010128 <kkr010128@icloud.com>
2024-07-26 16:14:23 +0900

    style: 사전 설정 컬러 정의 파일 추가
    서비스 개발과 관련된 시그니쳐 colors를 사전 정의하는 파일을 lib/에 생성합니다.

---
+++ lib/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// BLUE
+const omoBlue=Color(0xFF8793FF);
+
+// PINK
+const omoLightPink=Color(0xFFFFD9D7); 
+const omoDeepPink=Color(0xFFFF9AA6); 
+const omoSunsetPink=Color(0xFFEFB8C8); 
+
+
+// BLACK
+const omoGrey = Color(0xFFE6E6E6);
+const omoTextBlack = Color(0xFF4F4F4F);
