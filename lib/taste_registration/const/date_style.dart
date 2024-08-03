15636d1089050c7c8338f656b05692234d64f355
kkr010128 <kkr010128@icloud.com>
2024-08-03 15:39:12 +0900

    feat: 변수명 변경 및 메시지 리스트 수정
    - 정의된 취향 정보의 변수명을 API와의 통신을 위한 변수명으로 수정했습니다.
    - 챗봇의 메시지 리스트를 수정했습니다.

--- lib/taste_registration/const/date_style.dart
+++ lib/taste_registration/const/date_style.dart
@@ -1,18 +1,22 @@
 enum DateStyle {
-  passionateForFood("관광보다 먹방"),
-  cultureArtHistory("문화/예술/역사"),
-  shopping("쇼핑"),
-  relaxing("여유롭게 힐링"),
-  travelVibe("여행지 느낌 물씬"),
-  mustVisitTouristSpots("유명 관광지는 필수"),
-  exploreEverywhere("이곳 저곳 탐방"),
-  natureWithUs("자연과 함께"),
-  activities("체험/액티비티"),
-  relaxingAtOnePlace("한 곳에서 여유롭게"),
-  hotPlaces("핫플레이스"),
-  localRecommendation("현지인 추천");
+  PASSIONATE_FOR_FOOD("관광보다 먹방"),
+  CULTURE_ART_HISTORY("문화/예술/역사"),
+  SHOPPING("쇼핑"),
+  RELAXING("여유롭게 힐링"),
+  TRAVEL_VIBE("여행지 느낌 물씬"),
+  MUST_VISIT_TOURIST_SPOTS("유명 관광지는 필수"),
+  EXPLORE_EVERYWHERE("이곳 저곳 탐방"),
+  NATURE_WITH_US("자연과 함께"),
+  ACTIVITES("체험/액티비티"),
+  RELAXING_AT_ONE_SPOT("한 곳에서 여유롭게"),
+  HOT_PLACES("핫플레이스"),
+  LOCAL_RECOMMENDATION("현지인 추천");
 
   final String description;
 
   const DateStyle(this.description);
+  
+  String toApiString() {
+    return this.toString().split('.').last;
+  }
 }
